d009a68b9a5647c4154293909c29fa2b345a5914
DevHyperCoder <arduinoleo88@gmail.com>
2021-08-31 17:21:49 +0530

    enable cors ; enable SERVER_PORT

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+require("dotenv").config();
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
@@ -5,12 +6,13 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
 
   const swaggerConfig = new DocumentBuilder().setTitle('dgodo').build();
 
   const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('docs', app, swaggerDocument);
-  await app.listen(3000);
+  await app.listen(process.env.SERVER_PORT);
 }
 bootstrap();
